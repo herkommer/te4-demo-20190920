6de3d1a9955b1c004bd2f11ac0a55d0f0a9d9cb9
herkommer <m@herkommer.co.uk>
2019-09-20 10:18:20 +0200

    Added more Cars with different code styles

--- Objektorientering/MoreCars/Program.cs
+++ Objektorientering/MoreCars/Program.cs
@@ -10,6 +10,27 @@ namespace MoreCars
     {
         static void Main(string[] args)
         {
+            System.Collections.ArrayList Cars = new System.Collections.ArrayList();
+
+            Car c = new Car();
+            c.Make = "Volvo";
+            c.Model = "V70";
+            c.Color = "Black";
+
+            Cars.Add(c);
+
+            Car d = new Car() { Make="Audi", Model="A4", Color="White" };
+            Cars.Add(d);
+
+            Cars.Add(new Car() { Make = "BMW", Model = "i8", Color = "Red" });
+
+
+            Console.WriteLine("Vi har {0} st bilar i lager just nu", Cars.Count);
+
+            foreach (Car anka in Cars)
+            {
+                Console.WriteLine("{0} {1}", anka.Make,anka.Model);
+            }
         }
     }
 }
