23cd1eb8c04dd993f7f34862a6734b4cab9e03f1
herkommer <m@herkommer.co.uk>
2019-09-20 10:00:08 +0200

    Testing Car class as object

--- Objektorientering/CarRental/Program.cs
+++ Objektorientering/CarRental/Program.cs
@@ -10,12 +10,30 @@ namespace CarRental
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Creating Car, stand by.");
+
+            Car c;
+            c = new Car();
+
+            c.RegistrationNumber = "ABC123";
+            c.HorsePower = 170;
+            c.Color = "Black";
+
+            Console.WriteLine("Car created");
+            Console.ReadLine();
+
+            Console.WriteLine("We have a {0} car, with registration {1} and {2}hp",
+                                c.Color,
+                                c.RegistrationNumber,
+                                c.HorsePower);
         }
     }
 
     class Car
     {
         public string RegistrationNumber { get; set; }
+        public int HorsePower { get; set; }
+        public string Color { get; set; }
 
     }
 }
