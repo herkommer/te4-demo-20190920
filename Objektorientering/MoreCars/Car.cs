a7b3957cecc5e1703e06f613a654260add20c69d
herkommer <m@herkommer.co.uk>
2019-09-20 10:04:13 +0200

    Added new file for new Car class and new properties

---
+++ Objektorientering/MoreCars/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCars
+{
+    class Car
+    {
+        public string RegistrationNumber { get; set; }
+        public int HorsePower { get; set; }
+        public string Color { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+    }
+}
